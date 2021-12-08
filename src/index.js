012bb344ecb2278780e01d685603514b219ee444
Alicia AyersLott <amayers92@gmail.com>
2021-12-08 11:58:20 -0600

    adding login feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css'
+import { BrowserRouter as Router } from "react-router-dom"
+import { CareAlways } from "./components/CareAlways.js"
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CareAlways />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
